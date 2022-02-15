15f22b498f4f5964b1f46a681e16e1f5df4490a3
LukeMcCann <developer-lukemccann@outlook.com>
2022-02-15 22:11:24 +0000

    Horizontal movement example

--- app/src/game.mjs
+++ app/src/game.mjs
@@ -9,12 +9,22 @@ loadSprite('crapper', 'images/sprites/crapper.png');
 loadSprite('dirt', 'images/sprites/dirt.png');
 loadSprite('enemy', 'images/sprites/enemy.png');
 
-add([
+const CRAPPER_VELOCITY = 200;
+
+const crapper = add([
     pos(20, 280),
     sprite('crapper'),
     area(),
     body(),
 ]);
+
+keyDown('right', () => {
+    crapper.move(CRAPPER_VELOCITY, 0);
+});
+
+keyDown('left', () => {
+    crapper.move(-CRAPPER_VELOCITY, 0);
+});
 
 addLevel([
     "                           ",
